42fa4cc72e7eb68598ed3a47b43ea56a01fe77d3
Soni Limbu <limbusoni297@gmail.com>
2026-08-11 10:53:49 +0545

    Add student profile and Django backend updates

--- backend/students/admin.py
+++ backend/students/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import StudentProfile
 
-# Register your models here.
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "university",
+        "degree",
+        "career_interest",
+        "graduation_year",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+        "university",
+        "degree",
+    )
+
+    list_filter = (
+        "career_interest",
+        "graduation_year",
+    )
