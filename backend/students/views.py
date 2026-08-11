514bdc4b8a96f9212ad85c0d14ee1c9112c2223f
Soni Limbu <limbusoni297@gmail.com>
2026-08-11 11:17:03 +0545

    Add StudentProfile REST API

--- backend/students/views.py
+++ backend/students/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import StudentProfile
+from .serializers import StudentProfileSerializer
 
-# Create your views here.
+
+class StudentProfileViewSet(viewsets.ModelViewSet):
+    queryset = StudentProfile.objects.all()
+    serializer_class = StudentProfileSerializer
