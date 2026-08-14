6dafce7606f63196bb8c0a2044ff9239ef7643ae
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-08-14 18:55:30 +0545

    feat(student): add Certification CRUD

--- backend/src/main/java/com/careerplatform/backend/dto/request/ProjectRequest.java
+++ backend/src/main/java/com/careerplatform/backend/dto/request/ProjectRequest.java
@@ -1,4 +1,18 @@
 package com.careerplatform.backend.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProjectRequest {
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    private String description;
+    private String technologies;
+    private String projectUrl;
+    private String repoUrl;
 }
